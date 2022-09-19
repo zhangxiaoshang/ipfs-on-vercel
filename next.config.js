35463f30d0e06df1976f0ec34055dcd57b3b597d
bolingboling <zhangxiaoshang66@163.com>
2022-09-19 18:08:19 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
